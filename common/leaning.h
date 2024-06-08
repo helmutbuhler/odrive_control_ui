4c622a2d146932cea317d21ffa2d6544db3af1dd
helmutbuhler <buhler@8gadgetpack.net>
2024-06-08 22:33:47 +0200

    Remove unneeded files

--- common/leaning.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-
-bool leaning_init();
-bool leaning_update();
-void leaning_close();
-
-void calculate_rotation_vel_range(float h, float v, float imu_side_angle, float& r_min, float& r_max);
